88282117c4dead97dbf96168dc181035ca2e9a08
Milan Sommerer <mso@hup.de>
2026-06-23 16:16:28 +0200

    h

--- CInstaller/Updater.cs
+++ CInstaller/Updater.cs
@@ -4,6 +4,8 @@ namespace CInstaller;
 
 public class Updater
 {
+    private static string GithubToken = "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";
+    
     private static Version GetCurrentVersion() => Assembly.GetExecutingAssembly().GetName().Version!;
     
     public static async Task<bool> CheckForUpdate()
@@ -11,7 +13,9 @@ public class Updater
         var currentVersion = GetCurrentVersion();
         
         //TODO 1. make release 2. get service token for access
-        var response = await Installer.FindLatestGithubRelease("Kiirb", "CInsatller");
+        var response = await Installer.FindLatestGithubRelease("Kiirb", "CInsatller", GithubToken);
+        
+        Console.WriteLine(response.Content);
 
         return response.Version > currentVersion;
     }
