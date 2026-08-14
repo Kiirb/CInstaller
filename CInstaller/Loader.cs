eaa1ec8a7fbde40c9f166a0b830a05d7faa011ce
Kirb <Kiirb@outlook.de>
2026-08-14 20:40:13 +0200

    UT WoRK

--- CInstaller/Loader.cs
+++ CInstaller/Loader.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using System.IO;
+using System.Windows;
 
 namespace CInstaller;
 
@@ -7,12 +8,13 @@ public class Loader
 {
     public void LoaderRun()
     {
-        string gameFolder = AppContext.BaseDirectory;
+        string gameFolder = Path.GetDirectoryName(Environment.ProcessPath)!;
         string gameExe = Path.Join(gameFolder, Installer.GameName + ".exe");
 
         if (Path.Exists(gameExe))
         {
             Process.Start(gameExe);
         }
+        Application.Current.Shutdown();
     }
 }
